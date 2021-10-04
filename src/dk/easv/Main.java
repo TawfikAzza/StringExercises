2e2398c4b0b58f13346c8af0e401ecf5bc0c094d
TawfikAzza <tawf0021@easv365.dk>
2021-10-04 19:31:08 +0200

    Initial commit

---
+++ src/dk/easv/Main.java
@@ -0,0 +1,25 @@
+package dk.easv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Assignments assignments = new Assignments();
+        assignments.exercise1();
+        assignments.exercise2();
+        assignments.exercise3();
+        assignments.exercise4();
+        assignments.exercise5();
+        assignments.exercise6();
+        assignments.exercise7();
+        assignments.exercise8();
+        assignments.exercise9();
+        assignments.exercise10();
+        assignments.exercise11();
+        assignments.exercise12();
+        assignments.exercise13();
+    }
+}
